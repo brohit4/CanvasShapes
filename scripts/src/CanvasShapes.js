16a1121a1867cc31811205c94587de01ffbbf78f
brohit4 <brohit4@gmail.com>
2013-10-08 21:56:26 +0530

    Canvas shapes skeleton

--- scripts/src/CanvasShapes.js
+++ scripts/src/CanvasShapes.js
@@ -22,18 +22,33 @@ CanvasShapes = function(params){
 	params = params || {};
 
 
+
+
 	/*
 		The new object that is being created is added to the shapes array
 	*/
-	if (CanVasShapes.shapes) {
+	if (CanvasShapes.shapes) {
 		CanvasShapes.shapes.push(me);
 	}
 };
 
 /**
-	An array in the CanVasShapes constructor to maintain all the list of shapes
+	An array in the CanvasShapes constructor to maintain all the list of shapes
 */
 CanvasShapes.shapes = [];
+
+CanvasShapes.shapeIndex = 0 ;
+
+/**
+	validShapes object is used to maintain all the valis shapes CanvasShapes supports
+	so that if any other different shape is passed there will be a error logged and
+	further processing is stopped
+*/
+Canvas.validShapes = {
+	'RECTANGLE': true,
+	'CIRCLE': true,
+	'TEXT': true
+};
 
 /**
 	addMethod is way to augment the CanvasShapes prototype,
@@ -49,7 +64,7 @@ CanvasShapes.addMethod = function(methodName, method){
 	draw function to draw the current shape at the shapes x,y
 	and with shapes dimensions if any
 */
-CanVasShapes.addMethod('draw', function() {
+CanvasShapes.addMethod('draw', function() {
 	var me = this;
 });
 
@@ -58,6 +73,33 @@ CanVasShapes.addMethod('draw', function() {
 	and redraws the current shape and as well as the effected shapes due
 	to the movement
 */
-CanVasShapes.addMethod('move', function(newx, newy) {
+CanvasShapes.addMethod('move', function(newx, newy) {
 	var me = this;
+});
+
+/**
+	contains method will check if the passed shape is phyiscally
+	contained in this shape
+
+	This method's main use is to determine whether the passed shape has to be 
+	redrawn due to this shapes movement or change of depth
+*/
+CanvasShapes.addMethod('contains', function(shape) {
+	
+});
+
+/**
+	bringToFront method will bring this shape above the passed shape.
+	This will lead to a redraw of the affected containers
+*/
+CanvasShapes.addMethod('bringToFront', function(shape) {
+	
+});
+
+/**
+	bringToFront method will move this shape below the passed in shape
+	This will lead to a redraw of the affected containers
+*/
+CanvasShapes.addMethod('moveToBackGround', function(shape) {
+	
 });
